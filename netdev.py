c50f2971d9c38d28cba0f83616c18481218c45a0
erthalion <9erthalion6@gmail.com>
2019-01-06 15:50:44 +0100

    Cleanup netdev script

--- netdev.py
+++ netdev.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 #
-# netdev   Track network devices, that are used to transmit data
+# netdev        Track how much data was transmitted per netword device
 #
-# usage: netdev [-p PID] [-d]
+# usage: netdev [-d]
 
 
 from __future__ import print_function
@@ -15,7 +15,79 @@ import signal
 import sys
 
 
-text = """
+traditional = [
+    (1024 ** 5, 'P'),
+    (1024 ** 4, 'T'),
+    (1024 ** 3, 'G'),
+    (1024 ** 2, 'M'),
+    (1024 ** 1, 'K'),
+    (1024 ** 0, 'B'),
+    ]
+
+def size(bytes, system=traditional):
+    """Human-readable file size.
+
+    Using the traditional system, where a factor of 1024 is used::
+
+    >>> size(10)
+    '10B'
+    >>> size(100)
+    '100B'
+    >>> size(1000)
+    '1000B'
+    >>> size(2000)
+    '1K'
+    >>> size(10000)
+    '9K'
+    >>> size(20000)
+    '19K'
+    >>> size(100000)
+    '97K'
+    >>> size(200000)
+    '195K'
+    >>> size(1000000)
+    '976K'
+    >>> size(2000000)
+    '1M'
+
+    Using the SI system, with a factor 1000::
+
+    >>> size(10, system=si)
+    '10B'
+    >>> size(100, system=si)
+    '100B'
+    >>> size(1000, system=si)
+    '1K'
+    >>> size(2000, system=si)
+    '2K'
+    >>> size(10000, system=si)
+    '10K'
+    >>> size(20000, system=si)
+    '20K'
+    >>> size(100000, system=si)
+    '100K'
+    >>> size(200000, system=si)
+    '200K'
+    >>> size(1000000, system=si)
+    '1M'
+    >>> size(2000000, system=si)
+    '2M'
+
+    """
+    for factor, suffix in system:
+        if bytes >= factor:
+            break
+    amount = int(bytes/factor)
+    if isinstance(suffix, tuple):
+        singular, multiple = suffix
+        if amount == 1:
+            suffix = singular
+        else:
+            suffix = multiple
+    return str(amount) + suffix
+
+
+bpf_text = """
 #include <linux/ptrace.h>
 
 struct key_t {
@@ -151,38 +223,40 @@ def print_event(cpu, data, size):
 
 def run(args):
     print("Attaching...")
-    debug = 4
-    bpf = BPF(text=text, debug=debug)
+    debug = 4 if args.debug else 0
+    bpf = BPF(text=bpf_text, debug=debug)
     attach(bpf)
     exiting = False
 
-    bpf["events"].open_perf_buffer(print_event)
+    if args.debug:
+        bpf["events"].open_perf_buffer(print_event)
 
     print("Listening...")
     while True:
         try:
             sleep(1)
-            bpf.perf_buffer_poll()
+            if args.debug:
+                bpf.perf_buffer_poll()
         except KeyboardInterrupt:
             exiting = True
             # as cleanup can take many seconds, trap Ctrl-C:
             signal.signal(signal.SIGINT, signal_ignore)
 
         if exiting:
+            print()
             print("Detaching...")
+            print()
             break
 
     print("Total")
-    for (k, v) in bpf.get_table('net_data_hash').items():
-        print('{}: {}'.format(k.device, v.value))
+    for (k, v) in bpf.get_table("net_data_hash").items():
+        print('{}: {}'.format(k.device.decode("ascii"), size(v.value)))
 
 
 def parse_args():
     parser = argparse.ArgumentParser(
-        description="Track netword devices to transmit data",
+        description="Track how much data was transmitted per netword device",
         formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument("-p", "--pid", type=int, default=-1,
-            help="trace this PID only")
     parser.add_argument("-d", "--debug", action='store_true', default=False,
             help="debug mode")
 
